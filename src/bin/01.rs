0e549504b84b6eb8c022910e8dc260dc009ca0a9
Tom Anderson <tom@ando.gq>
2024-12-01 19:40:53 +1100

    day 1 part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -37,7 +39,50 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    #[derive(Default)]
+    struct CounterEntry {
+        left: u32,
+        right: u32,
+    }
+
+    #[derive(Default)]
+    struct Counter(HashMap<u32, CounterEntry>);
+
+    impl Counter {
+        pub fn left(&mut self, n: u32) {
+            self.0.entry(n).or_default().left += 1;
+        }
+
+        pub fn right(&mut self, n: u32) {
+            self.0.entry(n).or_default().right += 1;
+        }
+
+        pub fn total(&self) -> u32 {
+            self.0
+                .iter()
+                .map(|(n, entry)| entry.left * (n * entry.right))
+                .sum()
+        }
+    }
+
+    Some(
+        input
+            .lines()
+            .map(|line| line.split_whitespace())
+            .map(|mut line| {
+                (
+                    line.next().unwrap().parse::<u32>().unwrap(),
+                    line.next().unwrap().parse::<u32>().unwrap(),
+                )
+            })
+            .fold(Counter::default(), |mut counter, (left, right)| {
+                counter.left(left);
+                counter.right(right);
+
+                counter
+            })
+            .total(),
+    )
 }
 
 #[cfg(test)]
@@ -53,6 +98,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
