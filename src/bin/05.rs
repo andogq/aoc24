df57366811aa4d640e8140c779d6d996b96cec35
Tom Anderson <tom@ando.gq>
2024-12-05 22:47:13 +1100

    day 5 part 2

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{cmp::Ordering, collections::HashMap};
 
 use itertools::Itertools;
 
@@ -50,7 +50,50 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (rules, pages) = input.split_once("\n\n").unwrap();
+
+    let rules = rules
+        .lines()
+        .map(|line| {
+            line.split("|")
+                .map(|n| n.parse::<u32>().unwrap())
+                .next_tuple()
+                .unwrap()
+        })
+        .fold(HashMap::new(), |mut map, (left, right)| {
+            map.insert((left, right), Order::Before);
+            map.insert((right, left), Order::After);
+
+            map
+        });
+
+    Some(
+        pages
+            .lines()
+            .map(|line| {
+                line.split(",")
+                    .map(|n| n.parse::<u32>().unwrap())
+                    .collect::<Vec<_>>()
+            })
+            .filter(|line| {
+                !line.iter().enumerate().all(|(i, left)| {
+                    line[i + 1..]
+                        .iter()
+                        .flat_map(|right| rules.get(&(*left, *right)))
+                        .all(|order| *order == Order::Before)
+                })
+            })
+            .map(|mut line| {
+                line.sort_unstable_by(|a, b| match rules.get(&(*a, *b)) {
+                    Some(Order::Before) => Ordering::Less,
+                    Some(Order::After) => Ordering::Greater,
+                    None => Ordering::Equal,
+                });
+                line
+            })
+            .map(|line| line[line.len() / 2])
+            .sum(),
+    )
 }
 
 #[cfg(test)]
@@ -66,6 +109,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(123));
     }
 }
