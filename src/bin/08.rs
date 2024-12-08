a6bfaef6c55dc57fb40103c76abcd5c5ad076e4c
Tom Anderson <tom@ando.gq>
2024-12-08 16:45:40 +1100

    clean up day 8

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -3,7 +3,12 @@ use std::collections::{HashMap, HashSet};
 
 advent_of_code::solution!(8);
 
-pub fn part_one(input: &str) -> Option<u32> {
+fn solve<F, I>(input: &str, get_points: F) -> u32
+where
+    F: Fn((usize, usize), [(usize, usize); 2]) -> I,
+    I: Iterator<Item = (usize, usize)>,
+{
+    // Determine the bounds of the map
     let (height, width) = {
         let mut lines = input.lines();
 
@@ -13,108 +18,78 @@ pub fn part_one(input: &str) -> Option<u32> {
         (height, width)
     };
 
-    Some(
-        input
-            .lines()
-            .enumerate()
-            .flat_map(|(y, line)| {
-                line.chars().enumerate().flat_map(move |(x, c)| match c {
-                    '.' => None,
-                    c => Some(((x, y), c)),
-                })
+    input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars().enumerate().flat_map(move |(x, c)| match c {
+                '.' => None,
+                c => Some(((x, y), c)),
             })
-            .fold(
-                HashMap::<char, Vec<(usize, usize)>>::new(),
-                |mut antennas, (pos, freqency)| {
-                    antennas.entry(freqency).or_default().push(pos);
+        })
+        // Group the antennas by frequency
+        .fold(
+            HashMap::<char, Vec<(usize, usize)>>::new(),
+            |mut antennas, (pos, freqency)| {
+                antennas.entry(freqency).or_default().push(pos);
 
-                    antennas
-                },
-            )
-            .values()
-            .flat_map(|antennas| {
-                antennas.iter().tuple_combinations().flat_map(|(a1, a2)| {
-                    let delta = (a2.0 as isize - a1.0 as isize, a2.1 as isize - a1.1 as isize);
+                antennas
+            },
+        )
+        .values()
+        // Generate individal combinations for each antenna
+        .flat_map(|antennas| antennas.iter().tuple_combinations())
+        // Call out to generate points for an antenna pair
+        .flat_map(|(a1, a2)| get_points((height, width), [*a1, *a2]))
+        // De-dupe and count resonance points
+        .collect::<HashSet<_>>()
+        .len() as u32
+}
 
-                    [
-                        (
-                            a1.0.checked_add_signed(-delta.0),
-                            a1.1.checked_add_signed(-delta.1),
-                        ),
-                        (
-                            a2.0.checked_add_signed(delta.0),
-                            a2.1.checked_add_signed(delta.1),
-                        ),
-                    ]
-                    .into_iter()
-                    .flat_map(|(x, y)| Some((x?, y?)))
-                    .filter(|(x, y)| *x < width && *y < height)
-                })
-            })
-            .collect::<HashSet<_>>()
-            .len() as u32,
-    )
+pub fn part_one(input: &str) -> Option<u32> {
+    Some(solve(input, |(height, width), [a1, a2]| {
+        let delta = (a2.0 as isize - a1.0 as isize, a2.1 as isize - a1.1 as isize);
+
+        [
+            (
+                a1.0.checked_add_signed(-delta.0),
+                a1.1.checked_add_signed(-delta.1),
+            ),
+            (
+                a2.0.checked_add_signed(delta.0),
+                a2.1.checked_add_signed(delta.1),
+            ),
+        ]
+        .into_iter()
+        .flat_map(|(x, y)| Some((x?, y?)))
+        .filter(move |(x, y)| *x < width && *y < height)
+    }))
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let (height, width) = {
-        let mut lines = input.lines();
+    Some(solve(input, |(height, width), [a1, a2]| {
+        let delta = (a2.0 as isize - a1.0 as isize, a2.1 as isize - a1.1 as isize);
 
-        let width = lines.next().unwrap().len();
-        let height = lines.count() + 1;
+        [(a1, -1), (a2, 1)].into_iter().flat_map(move |(p, d)| {
+            let delta = (delta.0 * d, delta.1 * d);
 
-        (height, width)
-    };
+            (0..)
+                .map(move |i| (delta.0 * i, delta.1 * i))
+                .map(move |(dx, dy)| (p.0.checked_add_signed(dx), p.1.checked_add_signed(dy)))
+                .map(move |(x, y)| {
+                    let x = x?;
+                    let y = y?;
 
-    Some(
-        input
-            .lines()
-            .enumerate()
-            .flat_map(|(y, line)| {
-                line.chars().enumerate().flat_map(move |(x, c)| match c {
-                    '.' => None,
-                    c => Some(((x, y), c)),
+                    if x < width && y < height {
+                        Some((x, y))
+                    } else {
+                        None
+                    }
                 })
-            })
-            .fold(
-                HashMap::<char, Vec<(usize, usize)>>::new(),
-                |mut antennas, (pos, freqency)| {
-                    antennas.entry(freqency).or_default().push(pos);
-
-                    antennas
-                },
-            )
-            .values()
-            .flat_map(|antennas| {
-                antennas.iter().tuple_combinations().flat_map(|(a1, a2)| {
-                    let delta = (a2.0 as isize - a1.0 as isize, a2.1 as isize - a1.1 as isize);
-
-                    [(*a1, -1), (*a2, 1)].into_iter().flat_map(move |(p, d)| {
-                        let delta = (delta.0 * d, delta.1 * d);
-
-                        (0..)
-                            .map(move |i| (delta.0 * i, delta.1 * i))
-                            .map(move |(dx, dy)| {
-                                (p.0.checked_add_signed(dx), p.1.checked_add_signed(dy))
-                            })
-                            .map(move |(x, y)| {
-                                let x = x?;
-                                let y = y?;
-
-                                if x < width && y < height {
-                                    Some((x, y))
-                                } else {
-                                    None
-                                }
-                            })
-                            .take_while(|p| p.is_some())
-                            .flatten()
-                    })
-                })
-            })
-            .collect::<HashSet<_>>()
-            .len() as u32,
-    )
+                .take_while(|p| p.is_some())
+                .flatten()
+        })
+    }))
 }
 
 #[cfg(test)]
