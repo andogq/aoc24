b794d1bce74f5bab8dbd8f1f442c7e09fad26324
Tom Anderson <tom@ando.gq>
2024-12-15 22:43:37 +1100

    day 14 part 2

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 
 advent_of_code::solution!(14);
@@ -77,6 +79,79 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
+    let mut items = input
+        .lines()
+        .map(|line| {
+            let (p, v) = line
+                .split_whitespace()
+                .map(|section| {
+                    section[2..]
+                        .split(",")
+                        .map(|n| n.parse::<i64>().unwrap())
+                        .collect_tuple::<(_, _)>()
+                        .unwrap()
+                })
+                .collect_tuple()
+                .unwrap();
+
+            ((p.0 as u64, p.1 as u64), v)
+        })
+        .collect::<Vec<_>>();
+
+    for i in 1.. {
+        items = items
+            .into_iter()
+            .map(|(mut position, velocity)| {
+                position = (
+                    (position.0 + size::WIDTH)
+                        .checked_add_signed(velocity.0)
+                        .unwrap()
+                        % size::WIDTH,
+                    (position.1 + size::HEIGHT)
+                        .checked_add_signed(velocity.1)
+                        .unwrap()
+                        % size::HEIGHT,
+                );
+
+                (position, velocity)
+            })
+            .collect();
+
+        let tree = (0..size::HEIGHT)
+            .map(|y| {
+                (0..size::WIDTH)
+                    .map(|x| (x, y))
+                    .skip_while(|pos| !items.iter().any(|(p, _)| pos == p))
+                    .take_while(|pos| items.iter().any(|(p, _)| pos == p))
+                    .count()
+            })
+            .filter(|&full| full >= 5)
+            .count()
+            >= 2;
+
+        if tree {
+            let mut grid = vec![vec![false; size::WIDTH as usize]; size::HEIGHT as usize];
+
+            for ((x, y), _) in items {
+                grid[y as usize][x as usize] = true;
+            }
+
+            println!("i: {i}");
+
+            println!(
+                "{}",
+                grid.into_iter()
+                    .map(|line| line
+                        .into_iter()
+                        .map(|c| if c { '#' } else { '.' })
+                        .collect::<String>())
+                    .join("\n")
+            );
+
+            return Some(i);
+        }
+    }
+
     None
 }
 
@@ -90,9 +165,9 @@ mod tests {
         assert_eq!(result, Some(12));
     }
 
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
+    // #[test]
+    // fn test_part_two() {
+    //     let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+    //     assert_eq!(result, None);
+    // }
 }
